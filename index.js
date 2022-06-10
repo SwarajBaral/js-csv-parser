f0f090093c6e00fd857d008d842695c682b6d9f8
SwarajBaral <baralswaraj40@gmail.com>
2022-06-10 23:16:20 +0530

    Added bootstrap, changed api route

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -8,47 +8,45 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/public/index.html");
-})
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/csv-to-json", (req, res) => {
-	try {
-		const csv = req.body.csv.trim();
-		const json = [];
-		const lines = csv.split(/\r?\n/);
-		console.log(lines);
-		const fields = lines[0].split(',');
-		
-		lines.shift();
+app.post("/api/csv-to-json", (req, res) => {
+  try {
+    const csv = req.body.csv.trim();
+    const json = [];
+    const lines = csv.split(/\r?\n/);
+    const fields = lines[0].split(",");
 
-		const convertToJson = (values) => {
-			let temp = {};
-			let j = 0;
-			for (var i of values.split(',')) {
-				temp[String(fields[j])] = i === "" ? null : i;
-				j++;
-			}
-			json.push(temp)
-		}
+    lines.shift();
 
-		for(var j of lines) {
-			convertToJson(j);
-		}
+    const convertToJson = (values) => {
+      let temp = {};
+      let j = 0;
+      for (var i of values.split(",")) {
+        temp[String(fields[j])] = i === "" ? null : i;
+        j++;
+      }
+      json.push(temp);
+    };
 
-		res.status(200).json({
-			"body" : json
-		});
-	} catch(err) {
-		res.status(500).send(err);
-	}
+    for (var j of lines) {
+      convertToJson(j);
+    }
 
-})
+    res.status(200).json({
+      body: json,
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 app.listen("8000", (err) => {
-	if(err){
-		console.log(err);
-	}
-	console.log("Server listening on 8000")
-})
+  if (err) {
+    console.log(err);
+  }
+  console.log("Server listening on 8000");
+});
